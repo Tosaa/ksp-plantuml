b732df5427c4f558f70198f391f286380e9d4abc
Tosaa <Tosaa@nomail.com>
2025-06-16 21:34:28 +0200

    Enable Maven local publishing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 
 dependencies {
     // Project "app" depends on project "utils". (Project paths are separated with ":", so ":utils" refers to the top-level "utils" project.)
-    ksp("tosaa.puml.ksp:pumlgenerator:1.0.0")
+    ksp("io.github.tosaa.puml.ksp:pumlgenerator:1.0.0")
 }
 
 val firstExampleConfiguration = mutableMapOf<String, String>().apply {
--- pumlgenerator/build.gradle.kts
+++ pumlgenerator/build.gradle.kts
@@ -1,9 +1,13 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     kotlin("jvm")
     id("maven-publish")
+    id("com.vanniktech.maven.publish") version "0.30.0"
+
 }
 
-group = "tosaa.puml.ksp"
+group = "io.github.tosaa.puml.ksp"
 version = "1.0.0"
 
 repositories {
@@ -24,8 +28,48 @@ kotlin {
     jvmToolchain(17)
 }
 
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    coordinates(group.toString(), "ksp-plantuml-generator", version.toString())
+
+    pom {
+        name = "ksp-plantuml-generator"
+        description = "A Kotlin Symbol processor for Plantuml diagram generation."
+        inceptionYear = "2025"
+        url = "https://github.com/tosaa/ksp-plantuml/"
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "tosaa-ksp-plantuml"
+                name = "Tosaa"
+                url = "https://github.com/tosaa/"
+            }
+        }
+        scm {
+            url = "https://github.com/tosaa/ksp-plantuml/"
+            connection = "scm:git:git://github.com/tosaa/ksp-plantuml.git"
+            developerConnection = "scm:git:ssh://git@github.com/tosaa/ksp-plantuml.git"
+        }
+    }
+}
+
+/*
 publishing {
+
+
+
     publications.create<MavenPublication>("ksp-puml-plugin").from(components["kotlin"])
 
     repositories.mavenLocal()
 }
+*/

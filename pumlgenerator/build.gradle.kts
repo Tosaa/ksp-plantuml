b68cb3267a1e1122f0c6c446e666d00f29f85659
Tosaa <Tosaa@nomail.com>
2025-08-23 20:22:39 +0200

    Use best compression and ignore comments when uploading PUML to server. Also make sure that only .puml files are upload.

--- pumlgenerator/build.gradle.kts
+++ pumlgenerator/build.gradle.kts
@@ -1,12 +1,15 @@
 import com.vanniktech.maven.publish.SonatypeHost
-import okhttp3.internal.toHexString
 import java.io.ByteArrayOutputStream
 import java.io.FileOutputStream
+import java.io.IOException
+import java.io.StringReader
+import java.lang.StringBuilder
 import java.net.HttpURLConnection
 import java.net.URL
 import java.util.zip.Deflater
 import kotlin.io.encoding.Base64
 import kotlin.io.encoding.ExperimentalEncodingApi
+
 
 plugins {
     kotlin("jvm")
@@ -23,10 +26,10 @@ repositories {
     mavenCentral()
 }
 
-ksp{
-    arg("puml.allowEmptyPackage","true")
-    arg("puml.showPropertyRelations","false")
-    arg("puml.showFunctionRelations","false")
+ksp {
+    arg("puml.allowEmptyPackage", "true")
+    arg("puml.showPropertyRelations", "false")
+    arg("puml.showFunctionRelations", "false")
 }
 
 dependencies {
@@ -99,13 +102,17 @@ val b64ToPlantuml: Map<Char, Char> = base64Alphabet.zip(plantumlAlphabet).associ
 fun compress(inputBytes: ByteArray): ByteArray {
 
     // Create Deflater instance with default compression level
-    val deflater = Deflater(Deflater.DEFAULT_COMPRESSION)
+    val deflater = Deflater(Deflater.BEST_COMPRESSION)
     deflater.setInput(inputBytes)
     deflater.finish()
 
     // Output buffer for compressed data
     val outputStream = ByteArrayOutputStream()
-    val buffer = ByteArray(2048)
+    var len: Int = inputBytes.size * 2
+    if (len < 1000) {
+        len = 1000
+    }
+    val buffer = ByteArray(len)
 
     // Perform compression
     while (!deflater.finished()) {
@@ -122,8 +129,23 @@ fun compress(inputBytes: ByteArray): ByteArray {
 
 @OptIn(ExperimentalEncodingApi::class)
 fun deflateAndEncode(text: String): String {
+    // Optimized text
+    val optimized = text.lines().mapNotNull {
+        var line = if (it.contains('\'')) {
+            it.replaceAfter('\'', "").replace("\'", "")
+        } else {
+            it
+        }
+        line = line.trim()
+        if (line.isEmpty() || line.isBlank()) {
+            null
+        } else {
+            line
+        }
+    }.joinToString("\n")
+    println(optimized)
     // Convert string to bytes using UTF-8
-    val inputBytes = text.toByteArray(Charsets.UTF_8)
+    val inputBytes = optimized.toByteArray(Charsets.UTF_8)
     val compressedBytes = compress(inputBytes)
     val compressedBytesSlice = compressedBytes.drop(2).dropLast(4).toByteArray()
     val compressedText = Base64.Default.encode(compressedBytesSlice)
@@ -132,8 +154,11 @@ fun deflateAndEncode(text: String): String {
 }
 
 fun createImageByServer(it: File) {
-    val plantumlBase64Encoded = deflateAndEncode(it.readText().trimIndent())
-    println(it.name)
+    if (!it.name.endsWith(".puml")) {
+        return
+    }
+    println("File: " + it.name)
+    val plantumlBase64Encoded = deflateAndEncode(it.readText())
     println("\tRequest image generation with ${plantumlBase64Encoded.length} payload")
     val url = URL("https://www.plantuml.com/plantuml/png/$plantumlBase64Encoded")
     val connection = url.openConnection() as HttpURLConnection
@@ -160,5 +185,6 @@ fun createImageByServer(it: File) {
     } else {
         println("\tFailed to download image for ${it.name}. HTTP response code: ${connection.responseCode}")
         println("\t${connection.responseMessage}")
+        println("\tURL: $url")
     }
 }
